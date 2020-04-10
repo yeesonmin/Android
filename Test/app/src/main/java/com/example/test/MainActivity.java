45607bfdc8a441dc396a1e645222706f84cf83d4
yeesonmin <yeesonmin@gmail.com>
2020-04-10 15:59:41 +0900

    Add Project[Test] (2.1~2.2)

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageView imageView2;
+
+    int imageIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+    }
+
+    public  void onClickBtn1(View v)
+    {
+        changeImage();
+    }
+
+    private void changeImage() {
+
+        if(imageIndex == 0)
+        {
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+            imageIndex = 1;
+        }else{
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+            imageIndex = 0;
+        }
+    }
+}
